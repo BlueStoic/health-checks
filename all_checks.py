95863fd832ee20918b82311291d2f28e571f4958
gdagostino <techdreams92@gmail.com>
2023-06-01 23:11:23 -0500

    It should be sys.PATH.exits.

--- all_checks.py
+++ all_checks.py
@@ -4,7 +4,7 @@ import sys
 
 def check_reebot():
 	"""Returns True if the computer has a pending reboot."""
-	return os.path.exist("/run/reoot-required")
+	return os.path.exists("/run/reboot-required")
 
 
 def main():
